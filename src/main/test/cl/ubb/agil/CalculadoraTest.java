35709a2774950e61da2f03aed5b888ee8813ab09
Miguel Castillo <m.castillo.vas@gmail.com>
2016-04-27 11:02:32 -0400

    Fallo primer test

--- src/main/test/cl/ubb/agil/CalculadoraTest.java
+++ src/main/test/cl/ubb/agil/CalculadoraTest.java
@@ -3,5 +3,15 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CalculadoraTest {
-
+	
+	@Test
+	public void sumarDosYTresEsCinco(){
+		/* Arrange*/
+		Calculadora calculadora = new Calculadora();
+		/* Act*/
+		int resul=calculadora.sumar(2,3);
+		/* Assert*/
+		assertThat(resul,equalTo(5));
+		
+	}
 }
