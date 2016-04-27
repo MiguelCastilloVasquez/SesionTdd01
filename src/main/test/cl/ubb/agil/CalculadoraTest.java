e8238bf6648cf2c500ba1a17a65fd654d7325e1b
Miguel Castillo <m.castillo.vas@gmail.com>
2016-04-27 11:11:12 -0400

    Falla segundo test

--- src/main/test/cl/ubb/agil/CalculadoraTest.java
+++ src/main/test/cl/ubb/agil/CalculadoraTest.java
@@ -17,4 +17,13 @@ public class CalculadoraTest {
 		assertThat(resul,equalTo(5));
 		
 	}
+	@Test
+	public void sumarMenosUnoYSieteEsSeis(){
+		/* Arrange*/
+		Calculadora calculadora = new Calculadora();
+		/* Act*/
+		int resul=calculadora.sumar(-1,7);
+		/* Assert*/
+		assertThat(resul,equalTo(6));
+	}
 }
