938747c82743671866d39778d138275c6b7bc53d
Miguel Castillo <m.castillo.vas@gmail.com>
2016-04-27 11:33:33 -0400

    Fallo quinto test

--- src/main/test/cl/ubb/agil/CalculadoraTest.java
+++ src/main/test/cl/ubb/agil/CalculadoraTest.java
@@ -1,16 +1,23 @@
 package cl.ubb.agil;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CalculadoraTest {
+	private Calculadora calculadora;
+	
+	@Before
+	public void setup(){
+		calculadora = new Calculadora();
+	}
 	
 	@Test
 	public void sumarDosYTresEsCinco(){
 		/* Arrange*/
-		Calculadora calculadora = new Calculadora();
+		
 		/* Act*/
 		int resul=calculadora.sumar(2,3);
 		/* Assert*/
@@ -20,7 +27,7 @@ public class CalculadoraTest {
 	@Test
 	public void sumarMenosUnoYSieteEsSeis(){
 		/* Arrange*/
-		Calculadora calculadora = new Calculadora();
+		
 		/* Act*/
 		int resul=calculadora.sumar(-1,7);
 		/* Assert*/
@@ -30,7 +37,7 @@ public class CalculadoraTest {
 	@Test
 	public void restarDocientosMenosSetentaYTresEsCientoVeinteYSiete(){
 		/* Arrange*/
-		Calculadora calculadora = new Calculadora();
+		
 		/* Act*/
 		int resul=calculadora.restar(200,73);
 		/* Assert*/
@@ -40,10 +47,19 @@ public class CalculadoraTest {
 	@Test
 	public void restarMenosDiezMenosCincoEsMenosQuince(){
 		/* Arrange*/
-		Calculadora calculadora = new Calculadora();
+		
 		/* Act*/
 		int resul=calculadora.restar(-10,5);
 		/* Assert*/
 		assertThat(resul,equalTo(-15));
+	}
+	@Test
+	public void multiplicarSeisPorTresEsDieciocho(){
+		/* Arrange*/
+		
+		/* Act*/
+		int resul=calculadora.multiplicar(6,3);
+		/* Assert*/
+		assertThat(resul,equalTo(18));
 	}
 }
